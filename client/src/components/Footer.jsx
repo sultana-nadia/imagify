e589acb65f849d84a1b50fb2691061e35c867c1a
Artreeus <142108262+Artreeus@users.noreply.github.com>
2026-05-23 22:26:13 +0600

    footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,21 @@
-import { assets } from "../assets/assets"
-
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div className="flex items-center justify-between gap-4 py-3 mt-20">
-
       <img src={assets.logo} className="" width={150} alt="" />
 
-      <p className=" flex-1 border-gray-400 border-l pl-4 text-sm text-gray-500 max-sm:hidden">Copyright @Greatstack.dev | All right reserved.</p>
+      <p className=" flex-1 border-gray-400 border-l pl-4 text-sm text-gray-500 max-sm:hidden">
+        Copyright Nadia Sultana | All right reserved.
+      </p>
 
       <div className="flex gap-2.5">
         <img src={assets.facebook_icon} width={35} alt="" />
         <img src={assets.twitter_icon} width={35} alt="" />
         <img src={assets.instagram_icon} width={35} alt="" />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
